ae065bd8e55bf0925cede2650742c80e8297efe1
Guillaume Chau <alphadelta.fg@gmail.com>
2019-11-13 12:39:10 +0100

    feat: initial release

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
 # window-button-layout
-Retrieve OS window buttons layout (close, maximize, minimize...)
+Retrieve OS window buttons layout (close, maximize, minimize...) depending on platform and user preferences. Useful to create a custom window title in Electron that matches the rest of the OS for a more coherent UX.
+
+Currently supported:
+
+- Windows
+- MacOS
+- KDE
+- Gnome
+- Pantheon (elementaryOS)
+
+## Installation
+
+```bash
+yarn add window-button-layout
+```
+
+## Usage
+
+**Must be used in a Node context!** In an electron app, you can use IPC to send the result to your frontend code.
+
+The `getWindowButtonLayout` function will return a `WindowButtonLayout` object:
+
+```js
+const { getWindowButtonLayout } = require('window-button-layout')
+
+console.log(getWindowButtonLayout())
+/*
+{
+  start: ['menu'],
+  end: ['minimize', 'maximize', 'close']
+}
+*/
+```
+
+Depending on the OS and user preferences, you will get different arrays for `start` (left-side) and `end` (right-side). All buttons are not necessarily used; for example, on Pantheon (elementaryOS) there are no `menu` and `minimize` window buttons.
+
+Interfaces:
+
+```ts
+enum WindowButton {
+  CLOSE = 'close',
+  MAXIMIZE = 'maximize',
+  MINIMIZE = 'minimize',
+  SPACER = 'spacer',
+  MENU = 'menu',
+}
+
+interface WindowButtonLayout {
+  start: WindowButton[]
+  end: WindowButton[]
+}
+```
+
+Unkown buttons (that are not in the `WindowButton` enum) will be ignored.
---
+++ src/index.ts
@@ -0,0 +1,105 @@
+import * as os from 'os'
+import { execSync } from 'child_process'
+import * as path from 'path'
+import * as fs from 'fs'
+
+export enum WindowButton {
+  CLOSE = 'close',
+  MAXIMIZE = 'maximize',
+  MINIMIZE = 'minimize',
+  SPACER = 'spacer',
+  MENU = 'menu',
+}
+
+export interface WindowButtonLayout {
+  start: WindowButton[]
+  end: WindowButton[]
+}
+
+export const DEFAULT_LAYOUT_WINDOWS = {
+  start: [WindowButton.MENU],
+  end: [
+    WindowButton.MINIMIZE,
+    WindowButton.MAXIMIZE,
+    WindowButton.CLOSE
+  ]
+}
+
+export const DEFAULT_LAYOUT_MACOS = {
+  start: [
+    WindowButton.CLOSE,
+    WindowButton.MINIMIZE,
+    WindowButton.MAXIMIZE,
+  ],
+  end: [
+    WindowButton.MENU,
+  ],
+}
+
+export const DEFAULT_LAYOUT = DEFAULT_LAYOUT_WINDOWS
+
+export function getWindowButtonLayout (): WindowButtonLayout {
+  const platform = os.platform()
+  if (platform === 'win32') {
+    return DEFAULT_LAYOUT_WINDOWS
+  } else if (platform === 'darwin') {
+    return DEFAULT_LAYOUT_MACOS
+  } else if (platform === 'linux') {
+    // KDE
+    if (process.env.GDMSESSION.startsWith('kde') || process.env.XDG_CURRENT_DESKTOP === 'KDE') {
+      return getKDEButtons()
+    }
+    // ElementaryOS
+    else if (process.env.GDMSESSION === 'pantheon') {
+      return getGnomeButtons('org.pantheon.desktop.gala.appearance button-layout')
+    }
+    // Gnome
+    else if (process.env.GSETTINGS_SCHEMA_DIR) {
+      return getGnomeButtons('org.gnome.desktop.wm.preferences button-layout')
+    }
+  }
+  return DEFAULT_LAYOUT
+}
+
+function filterButtons (values: string[]): WindowButton[] {
+  return values.filter(
+    (v: WindowButton) => Object.values(WindowButton).includes(v)
+  ) as WindowButton[]
+}
+
+function getGnomeButtons (key: string): WindowButtonLayout {
+  const result = execSync(`gsettings get ${key}`, {
+    encoding: 'utf8',
+  })
+  const [, value] = /'(.*)'/.exec(result)
+  const [start, end] = value.split(':')
+  return {
+    start: filterButtons(start.split(',')),
+    end: filterButtons(end.split(',')),
+  }
+}
+
+const KDE_BUTTONS = {
+  M: WindowButton.MENU,
+  I: WindowButton.MINIMIZE,
+  A: WindowButton.MAXIMIZE,
+  X: WindowButton.CLOSE
+}
+
+function getKDEButtons (): WindowButtonLayout {
+  const configFile = path.resolve(os.homedir(), '.config/kwinrc')
+  if (fs.existsSync(configFile)) {
+    const content = fs.readFileSync(configFile, { encoding: 'utf8' })
+    const [,start] = /ButtonsOnLeft=(.*)/.exec(content)
+    const [,end] = /ButtonsOnRight=(.*)/.exec(content)
+    return {
+      start: filterKDEButtons(start.split('')),
+      end: filterKDEButtons(end.split(''))
+    }
+  }
+  return DEFAULT_LAYOUT
+}
+
+function filterKDEButtons (values: string[]): WindowButton[] {
+  return values.map(v => KDE_BUTTONS[v]).filter(v => !!v)
+}
